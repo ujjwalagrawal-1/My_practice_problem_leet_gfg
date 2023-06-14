48c4722caceac70e18082ed28018a6d8cf0839c9
ujjwalexcodie <122930278+ujjwalexcodie@users.noreply.github.com>
2023-06-14 22:41:13 +0530

    Time: 312 ms (5.99%), Space: 71.2 MB (43.63%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void insertattail(ListNode* &head,ListNode* &tail,int data){
+        if(head == 0 || tail == 0){
+            ListNode* newnode = new ListNode(data);
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+        ListNode* newnode = new ListNode(data);
+        tail->next = newnode;
+        tail = newnode;
+    }
+    ListNode* sortList(ListNode* head) {
+        map<int,int>hash;
+        ListNode* temp = head;
+        while(temp != 0){
+            hash[temp->val]++;
+            temp = temp ->next;
+        }
+        ListNode* raja = NULL;
+        ListNode* tail = NULL;
+        for(auto i = hash.begin();i != hash.end();i++){
+            for(auto j = i->second ; j != 0;j--){
+                insertattail(raja,tail,i->first);
+            }
+        }
+        delete head;
+        return raja;
+    }
+};

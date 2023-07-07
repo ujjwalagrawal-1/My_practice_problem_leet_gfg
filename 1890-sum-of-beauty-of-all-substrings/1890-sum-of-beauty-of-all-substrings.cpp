9abd3dd258ee9fe543c292176713cd66732df8c4
ujjwalexcodie <122930278+ujjwalexcodie@users.noreply.github.com>
2023-07-07 23:49:55 +0530

    Time: 552 ms (20.16%), Space: 26.7 MB (7.07%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int beautifull_or_Not(const string& sub) {
-        map<char, int> hash;
-        for (char c : sub) {
-            hash[c]++;
-        }
-        int uniqueChars = hash.size();
+    // int beautifull_or_Not(const string& sub) {
+    //     map<char, int> hash;
+    //     for (char c : sub) {
+    //         hash[c]++;
+    //     }
+    //     int uniqueChars = hash.size();
         
-        if (uniqueChars == 1) {
-            return 0;
-        }
+    //     if (uniqueChars == 1) {
+    //         return 0;
+    //     }
         
-        int maxFrequency = 0;
-        int minFrequency = INT_MAX;
-        for (auto pair : hash) {
-            maxFrequency = max(maxFrequency, pair.second);
-            minFrequency = min(minFrequency, pair.second);
-        }
-        if (maxFrequency - minFrequency >= 1) {
-            return maxFrequency - minFrequency;
-        }
+    //     int maxFrequency = 0;
+    //     int minFrequency = INT_MAX;
+    //     for (auto pair : hash) {
+    //         maxFrequency = max(maxFrequency, pair.second);
+    //         minFrequency = min(minFrequency, pair.second);
+    //     }
+    //     if (maxFrequency - minFrequency >= 1) {
+    //         return maxFrequency - minFrequency;
+    //     }
         
-        return 0;
-    }
+    //     return 0;
+    // }
     
     int beautySum(const string& s) {
         int count = 0;
